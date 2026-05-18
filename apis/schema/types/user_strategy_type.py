24b750d4ccfccdb9aa4ab320ab80ae95e9d2d43c
Anil <anilmaity77@gmail.com>
2026-05-18 10:25:42 +0530

    query amd mutations added

--- apis/schema/types/user_strategy_type.py
+++ apis/schema/types/user_strategy_type.py
@@ -3,6 +3,7 @@
 from datetime import datetime
 import graphene
 from graphene_django import DjangoObjectType
+from django.db.models import Q
 from pytz import timezone
 
 
@@ -10,6 +11,21 @@ from apis.models import (UserStrategy)
 from apis.schema.types.position_type import PositionType
 
 kolkata = timezone("Asia/Kolkata")
+
+
+def _positions_qs(self, date, userstrategy_ids):
+    """Open positions (quantity != 0) are always returned regardless of
+    date; closed positions are filtered to the selected date (defaults to
+    today IST when not given). userstrategy_ids further narrows the set
+    when present.
+    """
+    qs = self.position_set.all()
+    if userstrategy_ids:
+        qs = qs.filter(user_strategy__in=userstrategy_ids)
+    target_date = date if date else datetime.now(tz=kolkata).date()
+    return qs.filter(Q(created_at__date=target_date) | ~Q(quantity=0)).order_by(
+        "-created_at"
+    )
 
 ##############################################################################################################################################################
 
@@ -30,37 +46,31 @@ class UserStrategyType(DjangoObjectType):
         exclude = ( "position_set",)
     def resolve_name(self, info):
         return (self.strategy.name)
-    def resolve_total_position_count(self, info ,date = "", userstrategy_ids=[]):
-        if len(userstrategy_ids) > 0:
-            positions = self.position_set.filter(user_strategy__in=userstrategy_ids, date=date).order_by("-created_at")
-        elif date != "":
-            positions = self.position_set.filter(created_at__date=date).order_by("-created_at")
-        else:
-            positions = self.position_set.filter(created_at__date=datetime.now(tz=kolkata).date()).order_by("-created_at")
+    def resolve_total_position_count(self, info, date="", userstrategy_ids=[]):
+        return _positions_qs(self, date, userstrategy_ids).count()
 
-        return positions.count()
+    def resolve_active_positions_count(self, info, date="", userstrategy_ids=[]):
+        # Open positions (quantity != 0) regardless of date.
+        qs = self.position_set.exclude(quantity=0)
+        if userstrategy_ids:
+            qs = qs.filter(user_strategy__in=userstrategy_ids)
+        return qs.count()
 
-    def resolve_active_positions_count(self, info ,date = "", userstrategy_ids=[]):
-        if len(userstrategy_ids) > 0:
-            positions = self.position_set.filter(user_strategy__in=userstrategy_ids, date=date).order_by("-created_at")
-        elif date != "":
-            positions = self.position_set.filter(created_at__date=date).order_by("-created_at")
-        else:
-            positions = self.position_set.filter(created_at__date=datetime.now(tz=kolkata).date()).order_by("-created_at")
-        return positions.exclude(quantity=0).count()
-
-    def resolve_total_profit_loss(self, info ,date = "", userstrategy_ids=[]):
-
-        if date != "":
-            positions = self.position_set.filter(created_at__date=date).order_by("-created_at").all()
-        else:
-            positions = self.position_set.filter(created_at__date=datetime.now(tz=kolkata).date()).order_by("-created_at").all()
-
-        profit_loss = sum([(float(position.currencypair.ltp) - float(position.avg_buy_price))*float(position.quantity) + float(position.realized_profit_loss) for position in positions])
-        if profit_loss:
-            return profit_loss
-        else:
-            return 0
+    def resolve_total_profit_loss(self, info, date="", userstrategy_ids=[]):
+        # Match the per-position formula in PositionType.resolve_profit_loss
+        # so the strategy total agrees with the row-level numbers.
+        # The *100 is XAU contract size (1 lot = 100 oz). When the platform
+        # adds non-gold instruments, move this multiplier onto CurrencyPair.
+        positions = _positions_qs(self, date, userstrategy_ids)
+        return sum(
+            (
+                float(p.realized_profit_loss)
+                + (float(p.currencypair.ltp) - float(p.avg_buy_price))
+                * float(p.quantity)
+            )
+            * 100
+            for p in positions
+        )
 
 
     # get specific broker name
@@ -69,15 +79,8 @@ class UserStrategyType(DjangoObjectType):
 
     # fetching position values for user
 
-    def resolve_positions(self, info, date = "", userstrategy_ids=[]):
-        # give all position value of that user
-        # print(date)
-        if date != "":
-            positions = self.position_set.filter(created_at__date=date,user_strategy__in=userstrategy_ids).order_by("-created_at")
-        else:
-            positions = self.position_set.filter(created_at__date=datetime.now(tz=kolkata,).date(),user_strategy__in=userstrategy_ids).order_by("-created_at")
-
-        return positions
+    def resolve_positions(self, info, date="", userstrategy_ids=[]):
+        return _positions_qs(self, date, userstrategy_ids)
 
     # fetching daily positions
     def resolve_dailypositions(self, info ):
